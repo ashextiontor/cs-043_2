009e0fc79529fcc867433021feca51a8de9f01d4
ashextiontor <126810789+ashextiontor@users.noreply.github.com>
2023-03-09 14:11:20 -0500

    3/9/2023

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 
 import time
 
+speed1 = 1
+speed2 = 0.01
+speed3 = 0.0001
+speed4 = 0.00001
 
 print('this code is important')
 
 print('IT')
 
 while True:
-    print('the')
-    time.sleep(1)
-    
+    print(speed1)
+
+    print()
+    time.sleep(speed1)
+    speed1 += speed2
+    speed2 += speed3
+    speed3 += speed4
+    speed4 += 0.01
